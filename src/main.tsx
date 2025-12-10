0974587f938b6e5d73f8a26a8ec628c035a8e914
Steven2Oking <stevenkilonda1@gmail.com>
2025-12-10 10:21:16 +0100

    Ajout des modifications de balises Meta pour le Seo au fichiers index.html et aussi des dépendances HelmetProvider au main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <HelmetProvider>
     <App />
-  </StrictMode>
+  </HelmetProvider>
 );
